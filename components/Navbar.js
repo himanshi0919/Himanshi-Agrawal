b3e83cf429d001481cec935e4accd6c96bf87838
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 21:19:09 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,16 +6,16 @@ const Navbar = () => {
   
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a href="/"className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <span className="ml-12 text-xl  absolute top-[10px] left-[100px]" style={{ fontSize: '35px', color: 'black', fontWeight: 'bold'}}>A+</span>
+    <span className="ml-12 text-xl  absolute top-[10px] left-[70px]" style={{ fontSize: '35px', color: 'black', fontWeight: 'bold'}}>A+</span>
   <span className="ml-2 text-xl  absolute top-[10px] left-[130px]" style={{ fontSize: '35px', color: 'black' }}>Studio</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <a href="/" className="mr-10">Home</a>
-      <a class="mr-10">What We Do</a>
-      <a class="mr-10">Service</a>
-      <a class="mr-10">Project</a>
-      <a class="mr-10">Blog</a>
-      <a class="mr-10">Contact</a>
+      <a className="mr-10 absolute top-[10px] left-[180px]">What We Do</a>
+      <a className="mr-10 absolute top-[10px] left-[200px]">Service</a>
+      <a className="mr-10 absolute top-[10px] left-[220px]">Project</a>
+      <a className="mr-10 absolute top-[10px] left-[240px]">Blog</a>
+      <a className="mr-10 absolute top-[10px] left-[260px]">Contact</a>
     </nav>
    
   </div>
