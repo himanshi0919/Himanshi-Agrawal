1a4fc6259561b0f5b6e3508711bf3e5c5125b4b6
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 21:15:45 +0530

    Update1 Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,8 @@ const Navbar = () => {
   
   <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a href="/"className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <span className="ml-12 text-xl" style={{ fontSize: '35px', color: 'black', fontWeight: 'bold' } absolute top-[10px] left-[100px]}>A+</span>
-  <span className="ml-2 text-xl" style={{ fontSize: '35px', color: 'black' } absolute top-[10px] left-[130px]}>Studio</span>
+    <span className="ml-12 text-xl  absolute top-[10px] left-[100px]" style={{ fontSize: '35px', color: 'black', fontWeight: 'bold'}}>A+</span>
+  <span className="ml-2 text-xl  absolute top-[10px] left-[130px]" style={{ fontSize: '35px', color: 'black' }}>Studio</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
       <a href="/" className="mr-10">Home</a>
