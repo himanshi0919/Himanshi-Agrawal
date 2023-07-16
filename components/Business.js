b2644228626d8b048c502b07ae958379be6019a3
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 18:37:00 +0530

    Update Business.js

--- components/Business.js
+++ components/Business.js
@@ -5,20 +5,20 @@ const Business = () => {
    
     <div className="px-4 lg:px-16 py-10 rounded-3xl">
        <img className="absolute top-[1520px] left-[1015px]  w-[140px] h-[140px]"
-    alt="" src="/assets/Ellipse 87.png"/>
+    alt="" src="../assets/Ellipse 87.png"/>
     <img className="absolute top-[1090px] left-[45px] w-[150px] h-[150px] "
-    alt="" src="/assets/Rectangle 25.png"/>
+    alt="" src="../assets/Rectangle 25.png"/>
     
    
     <img
     className="absolute top-[1455px] left-[64px] w-[405px] h-[154px] "
     alt=""
-    src="/assets/Group 70.png"/>
+    src="../assets/Group 70.png"/>
     
     
    <img
     className="absolute top-[950px] left-[340px]  w-[950px] h-[619px]"
-    alt="" src="/assets/Rectangle 24.png"/>
+    alt="" src="../assets/Rectangle 24.png"/>
     <div className="flex flex-col gap-4 lg:gap-2">
     <p className="text-2xl sm:text-6xl md:text-10xl lg:text-4xl font-medium fontWeight-bold absolute top-[1140px] left-[80px] w-[580px] h-[185px]">How can we help
     <br className="lg:flex hidden" />
@@ -32,11 +32,11 @@ const Business = () => {
     
     
     <img className="absolute top-[855px] left-[500px]  w-[340px] h-[450px]"
-    alt="" src="/assets/Business1.png"/>
+    alt="" src="../assets/Business1.png"/>
     <img className="absolute top-[940px] left-[610px]  w-[100px] h-[100px]"
-    alt="" src="/assets/blue.png"/>
+    alt="" src="../assets/blue.png"/>
    <img className="absolute top-[960px] left-[627px] w-[64px] h-[64px] overflow-hidden"
-      alt="" src="/assets/box-search 1.png"/>
+      alt="" src="../assets/box-search 1.png"/>
        <div className="flex flex-col gap-4 lg:gap-2">
         <p className="text-[20px] font-medium fontWeight-bold absolute top-[1050px] left-[600px] w-[167px] h-[60px]">
         Business Idea </p>
@@ -50,11 +50,11 @@ const Business = () => {
         discuss niffty-gritty like</p>
            
     <img className="absolute top-[790px] left-[800px]  w-[360px] h-[450px]"
-    alt="" src="/assets/Business1.png"/>
+    alt="" src="../assets/Business1.png"/>
      <img className="absolute top-[890px] left-[920px]  w-[100px] h-[100px]"
-    alt="" src="/assets/Rectangle 28.png"/>
+    alt="" src="../assets/Rectangle 28.png"/>
     <img className="absolute top-[910px] left-[940px] w-[64px] h-[64px] overflow-hidden"
-      alt="" src="/assets/empty-wallet 1.png"/>
+      alt="" src="../assets/empty-wallet 1.png"/>
        <div className="flex flex-col gap-4 lg:gap-2">
        <p className="text-[20px] font-medium fontWeight-bold absolute top-[1000px] left-[925px] w-[167px] h-[60px]">
        Financial </p>
@@ -69,11 +69,11 @@ const Business = () => {
          models, pricing billing
         </p>
         <img className="absolute top-[1270px] left-[500px]  w-[360px] h-[450px]"
-    alt="" src="/assets/Business1.png"/>
+    alt="" src="../assets/Business1.png"/>
     <img className="absolute top-[1350px] left-[632px]  w-[100px] h-[100px]"
-    alt="" src="/assets/develop.png"/>
+    alt="" src="../assets/develop.png"/>
     <img className="absolute top-[1370px] left-[650px]  w-[64px] h-[64px]"
-    alt="" src="/assets/code-1 1.png"/>
+    alt="" src="../assets/code-1 1.png"/>
       <div className="flex flex-col gap-4 lg:gap-2">
      <p className="text-[20px] font-medium fontWeight-bold absolute top-[1480px] left-[620px] w-[167px] h-[60px]">
     Development
@@ -91,9 +91,9 @@ const Business = () => {
       <img className="absolute top-[1210px] left-[800px]  w-[360px] h-[450px]"
     alt="" src="/assets/Business1.png"/>
     <img className="absolute top-[1300px] left-[925px]  w-[100px] h-[100px]"
-    alt="" src="/assets/market.png"/>
+    alt="" src="../assets/market.png"/>
     <img className="absolute top-[1320px] left-[945px]  w-[64px] h-[64px]"
-    alt="" src="/assets/chart-square 1.png"/>
+    alt="" src="../assets/chart-square 1.png"/>
      <div className="flex flex-col gap-4 lg:gap-2">
      <p className="text-[20px] font-medium fontWeight-bold absolute top-[1425px] left-[915px] w-[167px] h-[60px]">
      Market Analysis
