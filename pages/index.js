25fb5c029f448093f42a3655f864f3001829f1f5
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 20:42:54 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800;900&display=swap" ></link>
   <link rel="icon" href="/favicon.ico" />
   </Head>
-  <Hero/>
+ 
   <Footer/>
   <Navbar />
   </div>
