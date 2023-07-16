3f335e4f99c873ac2fba0c1e7b855248eac55fa7
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 17:47:16 +0530

    Update Subscribe.js

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Subscribe = () => {
   return (
     <div className="container mx-auto py-20">
-         <img className="absolute top-[3050px] left-[40px] w-[170px] h-[170px]" alt="" src="/assets/Rectangle 32.png"/>
-        <img className="absolute top-[2900px] left-[70px] w-[1096px] h-[282px]" alt="" src="/assets/Rectangle 30.png"/>
+         <img className="absolute top-[3050px] left-[40px] w-[170px] h-[170px]" alt="" src="./assets/Rectangle 32.png"/>
+        <img className="absolute top-[2900px] left-[70px] w-[1096px] h-[282px]" alt="" src="./assets/Rectangle 30.png"/>
         <div className="flex flex-col gap-2 lg:gap-1">
     <p className="text-2xl sm:text-4xl md:text-3xl lg:text-3xl font-medium fontWeight-bold text-black absolute top-[2970px] left-[200px] w-[386px] h-[49px] ">
     Subscribe Newsletter
@@ -12,9 +12,9 @@ const Subscribe = () => {
     <p className="text-[12px] lg:text-s font-medium text-gray-600 absolute top-[3020px] left-[200px] w-[588px] h-[28px] size-[18px]">
     I will update good news and promotion service not spam.
    </p>
-   <img className="absolute top-[2840px] left-[1070px] w-[155px] h-[144px]" alt="" src="/assets/Group 73.png"/>
-   <img className="absolute top-[2890px] left-[700px] w-[490px] h-[292px]" alt="" src="/assets/Rectangle 31.png"/>
-   <img className="absolute top-[2940px] left-[550px] w-[550px] h-[150px]" alt="" src="/assets/subscriber.png"/>
+   <img className="absolute top-[2840px] left-[1070px] w-[155px] h-[144px]" alt="" src="./assets/Group 73.png"/>
+   <img className="absolute top-[2890px] left-[700px] w-[490px] h-[292px]" alt="" src="./assets/Rectangle 31.png"/>
+   <img className="absolute top-[2940px] left-[550px] w-[550px] h-[150px]" alt="" src="./assets/subscriber.png"/>
    <p className="text-[16px] font-large absolute top-[2990px] left-[620px] w-[290px] h-[20px]">
    Enter your email address..
    </p>
