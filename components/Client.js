382edb42a1dfb8ec770f05fff517591cf87c2209
Himanshi Agrawal <116184971+himanshi0919@users.noreply.github.com>
2023-07-16 22:08:20 +0530

    Update Client.js

--- components/Client.js
+++ components/Client.js
@@ -18,9 +18,9 @@ const Client = () => {
 
     <div className="flex flex-row gap-10 items-center">
    <img className="absolute top-[720px] left-[500px]"src="./assets/Google Logo.png" alt=""/>
-   <img className="absolute top-[720px] left-[750px]" src="./assets/Airbnb Logo.png" alt=""/>
-   <img className="absolute top-[720px] left-[90px]" src="./assets/Uber Eats Logo.png" alt=""/>
-   <img className="absolute top-[720px] left-[1000px]" src="./assets/Amazon Logo.png" alt=""/>
+   <img className="absolute top-[720px] left-[690px]" src="./assets/Airbnb Logo.png" alt=""/>
+   <img className="absolute top-[720px] left-[750px]" src="./assets/Uber Eats Logo.png" alt=""/>
+   <img className="absolute top-[720px] left-[850px]" src="./assets/Amazon Logo.png" alt=""/>
           
         </div>
         </div> 
